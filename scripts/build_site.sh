91f4aa92399d00b32d1922499a1b3dcdae996fd8
ccn <ccn@cuppajoeman.com>
2025-12-10 13:22:14 -0500

    feat: add styles for generated index pages

--- scripts/build_site.sh
+++ scripts/build_site.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 python fast-html/main.py --base-dir ../html/ --gen-dir ../fh_generated_html --base-template-file cpp_manual_template.html --custom-template-conversion-file cpp_manual_template_conversion_logic.py 
-python fsweb/main.py -ifm merge -s ../fh_generated_html/ -o ../generated_html
+python fsweb/main.py -ifm merge -s ../fh_generated_html/ -o ../generated_html -css /styles.css
